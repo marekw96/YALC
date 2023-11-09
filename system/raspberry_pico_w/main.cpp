c166593731fc257a14eee5bb3763a1b58c6ecdb1
marekw96 <marek@wiecek.pro>
2023-11-09 21:27:16 +0100

    Compile with ws2812 support library

--- system/raspberry_pico_w/main.cpp
+++ system/raspberry_pico_w/main.cpp
@@ -2,9 +2,13 @@
 #include "pico/stdlib.h"
 
 #include "Time.hpp"
+#include "ws2812/ws2812.hpp"
 
 int main() {
     setup_default_uart();
+    WS2812_Init();
+
+
     printf("Hello, world!\n");
 
     Time timeProvider;
